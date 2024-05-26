cb8f9d28012c578084dcfad78d6c3c8a663b40b8
Mar1f <938224402@qq.com>
2024-05-26 12:48:02 +0800

    腾讯云配置-文件上传功能实现

--- yuzi-generator-web-backed/src/main/java/com/yupi/web/controller/FileController.java
+++ yuzi-generator-web-backed/src/main/java/com/yupi/web/controller/FileController.java
@@ -42,6 +42,37 @@ public class FileController {
     private CosManager cosManager;
 
     /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @PostMapping("/test//upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+    /**
      * 文件上传
      *
      * @param multipartFile
